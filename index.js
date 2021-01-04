645f040487c941e50bf39277538c98ab4e19aeb2
sarisgar28 <sarisgar28@gmail.com>
2021-01-04 14:35:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Add your code here
+fetch("http://localhost:3000/dogs", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify({
+    dogName: "Byron",
+    dogBreed: "Poodle"
+  })
+});
+
+let formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle"
+  };
+   
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+      };
+       
+      fetch("http://localhost:3000/dogs", configObj)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(object) {
+          console.log(object);
+        })
+        .catch(function(error) {
+          alert("Bad things! Ragnarők!");
+          console.log(error.message);
+        });
